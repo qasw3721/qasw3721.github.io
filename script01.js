13a99f2be806dcd4f7b00eeac946a76780c5341c
qasw3721 <squarechan2014@gmail.com>
2017-03-23 16:52:03 -0700

    3/23/217update

--- script01.js
+++ script01.js
@@ -1,45 +1,63 @@
 var door = 1;
 var gate=0;
-			function pointerLR()
-			{
-				if (door==1)
-				{
-					document.getElementById("menu").style.left="-240px";				
-					document.getElementById("pointerL").style.left="-240px";
-					
-					door = 0;
-				}
-				else
-				{
-					document.getElementById("menu").style.left="0px";
-					document.getElementById("pointerL").style.left="0px";
-					door = 1;
-				}
-			}
 			
-			function homepage()
-			{
-				window.location.href = "index.html";
-			}
+function pointerLR()
+{
+	if (door==1)
+	{
+		document.getElementById("menu").style.left="-240px";				
+		document.getElementById("pointerL").style.left="-240px";
+					
+		door = 0;
+	}
+	else
+	{
+		document.getElementById("menu").style.left="0px";
+		document.getElementById("pointerL").style.left="0px";
+		door = 1;
+	}
+}
 			
-			function zoom()
-			{
+function homepage()
+{
+	window.location.href = "index.html";
+}
+			
+function zoom()
+{
+	if (gate==0)
+	{
 
-				if (gate==0)
-				{
+	document.getElementById("rockman").style.left="240px";
+	document.getElementById("rockman").style.top="0px";
+	document.getElementById("rockman").style.width="640px";
+	document.getElementById("rockman").style.height="480px";
+	document.getElementById("rockman").style.cursor="default";
+	document.getElementById("close").style.left="860px";
+				
+	gate=1;
+	}
+}
+function close1()
+{
+	if (gate==1)
+	{
+	document.getElementById("rockman").style.left="400px";
+	document.getElementById("rockman").style.top="400px";
+	document.getElementById("rockman").style.width="120px";
+	document.getElementById("rockman").style.height="100px";
+	document.getElementById("rockman").style.cursor="pointer";
+	document.getElementById("close").style.left="-100px";	
+					
+	gate=0;
+					
+	}
+}
 
-					document.getElementById("rockman").style.width="640px";
-					document.getElementById("rockman").style.height="480px";
-					
-					gate=1;
+var num=100;
 
-				}
-				else
-				{
-					document.getElementById("rockman").style.width="120px";
-					document.getElementById("rockman").style.height="100px";
-					
-					gate=0;
-					
-				}
-			}
+function big()
+{
+	num+=10;
+	document.getElementById("square").style.fontSize="num";
+}
